71162d1c6df0ec87003892f0fbf2e19b205b421b
Mitch Malinin <mitchmalinin@gmail.com>
2021-11-19 22:38:38 -0500

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import type { NextPage } from "next"
-import Head from "next/head"
-import Image from "next/image"
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen grid gap-8 justify-items-center content-center bg-black  text-white">
-      <img src="../static/logo-bg.svg" />
-      <h1 className="text-4xl md:text-7xl">Big Boss Burgers</h1>
-      <h2 className="text-3xl md:text-5xl">Coming soon...</h2>
+    <div className="h-screen grid gap-6 justify-items-center content-center bg-black  text-white">
+      <img className=" w-10/12 md: max-w-sm" src="../static/logo-bg.svg" />
+      <h1 className="text-4xl md:text-5xl font-bold">Big Boss Burgers</h1>
+      <h2 className="text-3xl md:text-4xl font-light">Coming soon...</h2>
       <a
         className="text-blue-400 hover:text-white"
         href="https://twitter.com/BossBurgerNFT"
       >
         {" "}
-        <i className="fab fa-twitter fa-3x transition-all cursor-pointer"></i>
+        <i className="fab fa-twitter fa-2x transition-all cursor-pointer"></i>
       </a>
     </div>
   )
